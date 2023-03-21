cba41b9a23d2d906d6566ee9f0d75f24c0a402e1
Avondeil <123551851+Avondeil@users.noreply.github.com>
2023-03-21 15:47:13 +0500

    Add files via upload

---
+++ prakt13_2/Auto.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+namespace prakt13_2
+{
+    class Auto
+    {
+        private string Marka;
+        private string Model;
+        private string VIN;
+        private string Color;
+        private int Year;
+        public Auto(string Marka, string Model, string VIN, string Color, int Year)
+        {
+            this.Marka=Marka;
+            this.Model = Model;
+            this.VIN = VIN;
+            this.Color = Color;
+            this.Year = Year;
+        }
+        
+        public string getMarka()
+        {
+            return this.Marka;
+        }
+        public string getModel()
+        {
+            return this.Model;
+        }
+        public string getVIN()
+        {
+            return VIN;
+        }
+        public string getColor()
+        {
+            return this.Color;
+        }
+        public int getYear()
+        {
+            return this.Year;
+        }
+        public void setMarka(string Marka2)
+        {
+            this.Marka=Marka2;
+        }
+        public void setModel(string Model2)
+        {
+            this.Model = Model2;
+        }
+        public void setVIN(string VIN2)
+        {
+            this.VIN = VIN2;
+        }
+        public void setColor(string Color2)
+        {
+            this.Color = Color2;
+        }
+        public void setYear(int Year2)
+        {
+            this.Year = Year2;
+        }
+    }
+}
---
+++ prakt13_2/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace prakt13_2
+{
+    
+    public partial class Form1 : Form
+    {
+       // private Dictionary<int, Auto> Autolist = new Dictionary<int,Auto>();
+        private IList<Auto> Autolist = new List<Auto>();
+        public void addAuto(string Marka, string Model, string VIN, string Color, int Year)
+        {
+            Auto s  = new Auto(Marka, Model,VIN, Color, Year);
+            Autolist.Add(s);
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            showListInGrid();
+        }
+        //Удаление студента с коллекции
+        private void deleteStudent(int elementIndex)
+        {
+            Autolist.RemoveAt(elementIndex);
+            showListInGrid();
+        }
+        //Отображение колекции в таблице
+        private void showListInGrid()
+        {
+            dataGridView1.Rows.Clear();
+            foreach (Auto s in Autolist)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                DataGridViewTextBoxCell cell1 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell2 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell3 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell4 = new
+                DataGridViewTextBoxCell();
+                DataGridViewTextBoxCell cell5 = new
+                DataGridViewTextBoxCell();
+                cell1.ValueType = typeof(string);
+                cell1.Value = s.getMarka();
+                cell2.ValueType = typeof(string);
+                cell2.Value = s.getModel();
+                cell3.ValueType = typeof(int);
+                cell3.Value = s.getVIN();
+                cell4.ValueType = typeof(string);
+                cell4.Value = s.getColor();
+                cell5.ValueType = typeof(int);
+                cell5.Value = s.getYear();
+                row.Cells.Add(cell1);
+                row.Cells.Add(cell2);
+                row.Cells.Add(cell3);
+                row.Cells.Add(cell4);
+                row.Cells.Add(cell5);
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+
+        private DataGridViewColumn dataGridViewColumn1 = null;
+        private DataGridViewColumn dataGridViewColumn2 = null;
+        private DataGridViewColumn dataGridViewColumn3 = null;
+        private DataGridViewColumn dataGridViewColumn4 = null;
+        private DataGridViewColumn dataGridViewColumn5 = null;
+       
+
+        private void initDataGridView()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Columns.Add(getDataGridViewColumn1());
+            dataGridView1.Columns.Add(getDataGridViewColumn2());
+            dataGridView1.Columns.Add(getDataGridViewColumn3());
+            dataGridView1.Columns.Add(getDataGridViewColumn4());
+            dataGridView1.Columns.Add(getDataGridViewColumn5());
+            dataGridView1.AutoResizeColumns();
+        }
+        //Динамическое создание первой колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn1()
+        {
+            if (dataGridViewColumn1 == null)
+            {
+                dataGridViewColumn1 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn1.Name = "";
+                dataGridViewColumn1.HeaderText = "Марка";
+                dataGridViewColumn1.ValueType = typeof(string);
+                dataGridViewColumn1.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn1;
+        }
+        //Динамическое создание второй колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn2()
+        {
+            if (dataGridViewColumn2 == null)
+            {
+                dataGridViewColumn2 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn2.Name = "";
+                dataGridViewColumn2.HeaderText = "Модель";
+                dataGridViewColumn2.ValueType = typeof(string);
+                dataGridViewColumn2.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn2;
+        }
+        //Динамическое создание третей колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn3()
+        {
+            if (dataGridViewColumn3 == null)
+            {
+                dataGridViewColumn3 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn3.Name = "";
+                dataGridViewColumn3.HeaderText = "VIN";
+                dataGridViewColumn3.ValueType = typeof(string);
+                dataGridViewColumn3.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn3;
+        }
+        //Динамическое создание четвертой колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn4()
+        {
+            if (dataGridViewColumn4 == null)
+            {
+                dataGridViewColumn4 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn4.Name = "";
+                dataGridViewColumn4.HeaderText = "Цвет";
+                dataGridViewColumn4.ValueType = typeof(string);
+                dataGridViewColumn4.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn4;
+        }
+        //Динамическое создание пятой колонки в таблице
+        private DataGridViewColumn getDataGridViewColumn5()
+        {
+            if (dataGridViewColumn5 == null)
+            {
+                dataGridViewColumn5 = new DataGridViewTextBoxColumn();
+                dataGridViewColumn5.Name = "";
+                dataGridViewColumn5.HeaderText = "Год выпуска";
+                dataGridViewColumn5.ValueType = typeof(string);
+                dataGridViewColumn5.Width = dataGridView1.Width / 3;
+            }
+            return dataGridViewColumn5;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            initDataGridView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addAuto(textBox1.Text, textBox2.Text,textBox3.Text, textBox4.Text, Convert.ToInt32(textBox5.Text));
+        }
+
+        private void удалитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int selectedRow = dataGridView1.SelectedCells[0].RowIndex;
+            DialogResult dr = MessageBox.Show("Удалить автомобиль?", "",MessageBoxButtons.YesNo);
+            if (dr == DialogResult.Yes)
+            {
+                try
+                {
+                    deleteStudent(selectedRow);
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+    }
+}
+
